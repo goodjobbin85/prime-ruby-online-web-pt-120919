b3bdfafd247b38856db36201c595e3b07e3c4006
thomas dubiel <td1729@hotmail.com>
2019-12-21 23:49:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,35 @@
 # Add  code here!
 def prime?(number)
-  n = 2 
-  while n < number
-    return false if num % n == 0
-    n += 1 
-  end 
-  true 
+  count = 0 
+  if number == 0 
+    puts "0 is not prime" 
+  else 
+    n = 2 
+    while n < number
+      return false if num % n == 0
+      n += 1 
+    end 
+    true 
+end 
+
+=begin 
+count=0
+if (num==0)
+	puts "0 is not prime"
+else
+	
+	i=2
+	while(i<num)
+		if (num%i==0)
+			count+=1
+		end
+		i+=1
+	end
+	
 end
+	if count>1
+		puts "#{num} is not a prime number"
+	else
+		puts "#{num} is a prime number"
+	end
+=end
